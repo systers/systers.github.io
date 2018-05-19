c8ba412c45c305a8f7f8d81794d23c74b81eeed2
BaaniLeen <baani.jolly@gmail.com>
2018-05-19 20:01:39 +0530

    Issue#181 Develop the Nav Bar

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+
+const routes: Routes = [
+	{
+		path: '',
+		component: HomeComponent
+	},
+	{
+		path: 'about',
+		component: AboutComponent
+	},
+	{
+		path: 'projects',
+		component: ProjectsComponent
+	}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/projects/projects.component.html
@@ -0,0 +1,1 @@
+<h1>PROJECTS PAGE</h1>
