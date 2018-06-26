2d8575bf018e2cb98509714c34fd5265c9539b78
anumehaagrawal <anuzenith29@gmail.com>
2018-06-26 16:39:04 +0530

    Improvers added

--- src/app/contributors/contributors.component.ts
+++ src/app/contributors/contributors.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Admins } from './admins';
 import { Maintainers } from './maintainers';
 import { Contri } from './committers';
-
+import { Improvers } from './improvers';
 @Component({
   selector: 'app-contributors',
   templateUrl: './contributors.component.html',
@@ -12,6 +12,7 @@ export class ContributorsComponent implements OnInit {
   admins = Admins;
   maint = Maintainers;
   contri = Contri;
+  imp = Improvers;
 
   constructor() { }
 
