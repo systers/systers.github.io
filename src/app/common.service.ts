eadd979903a13aa84245aa65ee24cea7e7e7b5fb
BaaniLeen <baani.jolly@gmail.com>
2018-08-01 08:05:24 +0530

    Removed duplicate /api/getUser bug

--- src/app/common.service.ts
+++ src/app/common.service.ts
@@ -12,15 +12,15 @@ export class CommonService {
   constructor(private http: Http) { }
 
   saveUser(user) {
-    return this.http.post('https://murmuring-refuge-48696.herokuapp.com/api/getUser/api/SaveUser/', user)
+    return this.http.post('https://murmuring-refuge-48696.herokuapp.com/api/SaveUser/', user)
       .map((response: Response) => response.json());
   }
   GetUser() {
-    return this.http.get('https://murmuring-refuge-48696.herokuapp.com/api/getUser/api/getUser/')
+    return this.http.get('https://murmuring-refuge-48696.herokuapp.com/api/getUser/')
       .map((response: Response) => response.json());
   }
   deleteUser(id) {
-    return this.http.post('https://murmuring-refuge-48696.herokuapp.com/api/getUser/api/deleteUser/', { 'id': id })
+    return this.http.post('https://murmuring-refuge-48696.herokuapp.com/api/deleteUser/', { 'id': id })
       .map((response: Response) => response.json());
   }
 }
